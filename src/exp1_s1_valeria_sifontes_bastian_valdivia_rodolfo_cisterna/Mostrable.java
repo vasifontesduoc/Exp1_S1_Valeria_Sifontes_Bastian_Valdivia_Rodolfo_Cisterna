822994d697b873d27007316308c32af80059d164
Valeria <va.sifontes@duocuc.cl>
2025-05-28 21:32:45 -0400

    commit con todos los cambios

---
+++ src/exp1_s1_valeria_sifontes_bastian_valdivia_rodolfo_cisterna/Mostrable.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exp1_s1_valeria_sifontes_bastian_valdivia_rodolfo_cisterna;
+
+/**
+ *
+ * @author VALERIA
+ */
+
+// esta es la clase para la interfaz
+public interface Mostrable {
+    void mostrarDatosCliente();
+}
